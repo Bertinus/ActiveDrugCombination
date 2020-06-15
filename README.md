d7890f72f6d3aa865f56a3fc41c622a41adccd95
bertinus <paul.f.bertin@gmail.com>
2020-06-15 15:06:34 -0400

    Added data loading scripts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Effect of drug combination prediction using Active Learning
+
+The data we use is downloaded from this page: http://snap.stanford.edu/decagon/
+
---
+++ get_data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Starting to download data for use in running.  This may take a few minutes..."
+echo ""
+
+cwd=$(echo $PWD)
+
+cd Data
+
+# Decagon data
+wget http://snap.stanford.edu/decagon/bio-decagon-ppi.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-targets.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-targets-all.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-combo.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-mono.tar.gz
+wget http://snap.stanford.edu/decagon/bio-decagon-effectcategories.tar.gz
+
+tar -xzf bio-decagon-ppi.tar.gz
+tar -xzf bio-decagon-targets.tar.gz
+tar -xzf bio-decagon-targets-all.tar.gz
+tar -xzf bio-decagon-combo.tar.gz
+tar -xzf bio-decagon-mono.tar.gz
+tar -xzf bio-decagon-effectcategories.tar.gz
+
+# NCI data
+wget https://wiki.nci.nih.gov/download/attachments/338237347/ComboCompoundSet.sdf?version=1&modificationDate=1493822360000&api=v2
+wget https://wiki.nci.nih.gov/download/attachments/338237347/ComboCompoundNames_small.txt?version=1&modificationDate=1493822467000&api=v2
+wget https://wiki.nci.nih.gov/download/attachments/338237347/ComboCompoundNames_all.txt?version=1&modificationDate=1493822512000&api=v2
+
+cd ${cwd}
+
+echo ""
+echo "Finished downloading data"
